39b31325d3f2a65e618504d2f84bb505fc8936e8
AbdulbakiDEMIR <baki_demir_bs@hotmail.com>
2023-11-28 11:06:52 +0300

    28.11.2023*11.06

--- create-account.php
+++ create-account.php
@@ -11,30 +11,36 @@
     <div id="login-main-page-background"></div>
     <div id="login-main-page";>
         <div class="container">
-            <div class="card bg-transparent mb-2">
+            <div class="card bg-transparent my-5 py-5">
                 <div class="login-card-background"></div>
                 <div class="card-body">
                     <h3 class="fw-bold title">Kayıt Ol</h3>
                     <form id="kaydol" action="">
-                        <label for="name">İsim</label>
-                        <input class="form-control mb-2 mt-1" type="text" name="name" id="name">
-                        <label for="lastName">Soyisim</label>
-                        <input class="form-control mb-2 mt-1" type="text" name="lastName" id="lastName">
-                        <label for="tckn">TCKN</label>
-                        <input class="form-control mb-2 mt-1 inputNumber" placeholder="12345678910" type="text" maxlength="11" minlength="11" name="tckn" id="logintckn">
-                        <label for="password">Şifre</label>
-                        <input class="form-control mb-2 mt-1" placeholder="En az 8 karakter" type="password" minlength="8" name="password" id="loginPassword">
-                        <label for="passwordCheck">Şifre Kontrol</label>
-                        <input class="form-control mb-2 mt-1" type="password" minlength="8" name="passwordCheck" id="loginPasswordCheck" title="Şifreler aynı değil">
-                        <p class="text-danger" id="passWar" style="display: none;">Şifre uyuşmuyor</p>
+                        <label class="mt-2" for="name">İsim</label>
+                        <input class="form-control mt-1" type="text" name="name" id="namei">
+                        <p class="text-danger mb-0 mt-1" id="nameWar" style="display: none;">*Lütfen isim girin</p>
+                        <label class="mt-2" for="lastName">Soyisim</label>
+                        <input class="form-control mt-1" type="text" name="lastName" id="lastName">
+                        <p class="text-danger mb-0 mt-1" id="lastNameWar" style="display: none;">*Lütfen soyisim girin</p>
+                        <label class="mt-2" for="tckn">TCKN</label>
+                        <input class="form-control mt-1 inputNumber" placeholder="12345678910" maxlength="11" type="text" name="tckn" id="logintckn">
+                        <p class="text-danger mb-0 mt-1" id="tcWar" style="display: none;">*Lütfen geçerli TC kimlik numaranızı girin</p>
+                        <label class="mt-2" for="password">Şifre</label>
+                        <input class="form-control mt-1" placeholder="En az 8 karakter" type="password" name="password" id="loginPassword">
+                        <p class="text-danger mb-0 mt-1" id="passWar" style="display: none;">*Şifre 8 karakterden büyük olmalı</p>
+                        <label class="mt-2" for="passwordCheck">Şifre Kontrol</label>
+                        <input class="form-control mt-1" type="password" name="passwordCheck" id="loginPasswordCheck">
+                        <p class="text-danger mb-0 mt-1" id="passCheckWar" style="display: none;">*Şifre uyuşmuyor</p>
                         <input class="mb-3" type="checkbox" name="showPass" id="showPass">
                         <label for="showPass" class="ms-2">Şifreyi Göster</label>
                         <br>
-                        <label for="tel">Tel</label>
-                        <input class="form-control mb-2 mt-1 inputNumber" type="tel" name="tel" id="tel" placeholder="(555) 555 55 55">
-                        <label for="date">Doğum Tarihi</label>
-                        <input class="form-control mb-4 mt-1" type="date" name="date" id="date">
-                        <button type="submit" class="btn btn-primary ms-auto">Kaydol</button>
+                        <label class="" for="tel">Tel</label>
+                        <input class="form-control mt-1 inputNumber" type="tel" name="tel" id="tel" maxlength="10" placeholder="(555) 555 55 55">
+                        <p class="text-danger mb-0 mt-1" id="telWar" style="display: none;">*Lütfen geçerli bir telefon numarası girin</p>
+                        <label class="mt-2" for="date">Doğum Tarihi</label>
+                        <input class="form-control mt-1" type="date" name="date" id="date">
+                        <p class="text-danger mb-0 mt-1" id="dateWar" style="display: none;">*Lütfen doğum tarihi girin</p>
+                        <button type="submit" class="btn btn-primary ms-auto mt-3">Kaydol</button>
                     </form>
                     <hr>
                 </div>
@@ -66,12 +72,89 @@ showCheckBox.addEventListener("click",function(){
     else{password.type = "password"; passwordCheck.type = "password";}
 });
 
+var name2 = document.getElementById("namei");
+var nameWar = document.getElementById("nameWar");
+var lastname = document.getElementById("lastName");
+var lastnameWar = document.getElementById("lastNameWar");
+var tckn = document.getElementById("logintckn");
+var tcWar = document.getElementById("tcWar"); 
 var passWar = document.getElementById("passWar");
+var passCheckWar = document.getElementById("passCheckWar");
+var tel = document.getElementById("tel");
+var telWar = document.getElementById("telWar"); 
+var date = document.getElementById("date");
+var dateWar = document.getElementById("dateWar");
 var kaydol = document.getElementById("kaydol");
 kaydol.addEventListener("submit",function(event){
     event.preventDefault();
-    if(password.value != passwordCheck.value){
-        passWar.style.display = "flex";
+    var error = false;
+    if(name2.value == ""){
+        name2.style.border = "1px solid rgb(245, 55, 55)";
+        nameWar.style.display = "flex";
+        error = true;
     }
+    else{
+        name2.style.border = "none";
+        nameWar.style.display = "none";
+    }
+    if(lastname.value == ""){
+        lastname.style.border = "1px solid rgb(245, 55, 55)";
+        lastnameWar.style.display = "flex";
+        error = true;
+    }
+    else{
+        lastname.style.border = "none";
+        lastnameWar.style.display = "none";
+    }
+    if(tckn.value == "" || tckn.value.length < 11){
+        tckn.style.border = "1px solid rgb(245, 55, 55)";
+        tcWar.style.display = "flex";
+        error = true;
+    }
+    else{
+        tckn.style.border = "none";
+        tcWar.style.display = "none";
+    }
+    if(password.value == "" || password.value.length < 8){
+        password.style.border = "1px solid rgb(245, 55, 55)";
+        passWar.style.display = "flex";
+        error = true;
+    }
+    else{
+        password.style.border = "none";
+        passWar.style.display = "none";
+    }
+    if(tel.value == "" || tel.value.length < 10){
+        tel.style.border = "1px solid rgb(245, 55, 55)";
+        telWar.style.display = "flex";
+        error = true;
+    }
+    else{
+        tel.style.border = "none";
+        telWar.style.display = "none";
+    }
+    if(date.value == ""){
+        date.style.border = "1px solid rgb(245, 55, 55)";
+        dateWar.style.display = "flex";
+        error = true;
+    }
+    else{
+        date.style.border = "none";
+        dateWar.style.display = "none";
+    
+    }
+    if(password.value != passwordCheck.value){
+        passwordCheck.style.border = "1px solid rgb(245, 55, 55)";
+        passCheckWar.style.display = "flex";
+        error = true;
+    
+    }
+    else{
+        passwordCheck.style.border = "none";
+        passCheckWar.style.display = "none";
+    
+    }
+    if(!error){kaydol.submit();}
+    
 });
 </script>
--- login.php
+++ login.php
@@ -10,16 +10,17 @@
 <body class="position-relative">
     <div id="login-main-page-background"></div>
     <div id="login-main-page";>
-        <div class="container">
+        <div class="container my-5 py-5">
             <div class="card bg-transparent mb-2">
                 <div class="login-card-background"></div>
                 <div class="card-body">
                     <h3 class="fw-bold title">Giriş Yap</h3>
-                    <form action="">
+                    <form action="" id="kaydol">
                         <label for="tckn">TCKN</label>
-                        <input class="form-control mb-2 mt-1 inputNumber" placeholder="12345678910" type="text" maxlength="11" minlength="11" name="tckn" id="logintckn" title="Geçerli bir TC Kimlik Numarası girin (11 hane)">
+                        <input class="form-control mb-2 mt-1 inputNumber" placeholder="12345678910" type="text" maxlength="11" name="tckn" id="logintckn">
                         <label for="password">Şifre</label>
-                        <input class="form-control mb-4 mt-1" type="password" name="password" id="loginPassword">
+                        <input class="form-control mb-2 mt-1" type="password" name="password" id="loginPassword">
+                        <p class="text-danger mb-2" style="display: none;" id="loginWar">*Hatalı Giriş</p>
                         <div class="d-flex"> 
                             <input class="mb-3" type="checkbox" name="showPass" id="showPass">
                             <label for="showPass" class="ms-2">Şifreyi Göster</label>
@@ -50,8 +51,28 @@ for (var i = 0; i < inputNumber.length ; i++){
 }
 var showCheckBox = document.getElementById("showPass");
 var password = document.getElementById("loginPassword");
+var loginWar = document.getElementById("loginWar");
 showCheckBox.addEventListener("click",function(){
     if(showCheckBox.checked){password.type = "text";}
     else{password.type = "password";}
 })
+
+var tckn = document.getElementById("logintckn");
+var kaydol = document.getElementById("kaydol");
+kaydol.addEventListener("submit",function(event){
+    event.preventDefault();
+    var error = false;
+    if(tckn.value == "" || tckn.value.length < 11){
+        tckn.style.border = "1px solid rgb(245, 55, 55)";
+        loginWar.style.display = "flex";
+        error = true;
+    }
+    else{
+        tckn.style.border = "none";
+        loginWar.style.display = "none";
+    }
+    
+    if(!error){kaydol.submit();}
+    
+});
 </script>
