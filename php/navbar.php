3a40c14fcf3a7cd55eec3d80680d0487af7aa0c8
AnLaKiii <kaankursatkilic@gmail.com>
2023-12-25 22:26:49 +0300

    25.12.2023-22.26

--- php/navbar.php
+++ php/navbar.php
@@ -9,17 +9,6 @@
         <a class="logo" href="./" >
             <img src="img/logo.png" alt="">
         </a>
-        <!--
-        <li class="">
-            <div id="search-box1">
-                <div id="input-box1">
-                    <input id="input1" type="text" placeholder="Search" on="deneme()" onkeyup="showHint(this.value)">
-                    <div id="write1"></div>
-                    <button id="search-button1"><i class="fa-solid fa-magnifying-glass"></i></button>
-                </div>
-            </div>
-        </li>
-        -->
         <li class="my-nav-item toggle-drop">
             <div class="user-image-1">
                 <img src="img/user.png" alt="">
